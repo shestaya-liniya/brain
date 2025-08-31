85dde9a674e7607ba0f1631bd80c78ff6d7cf4f1
shestaya_liniya <pochta5550@gmail.com>
2025-08-31 18:29:05 +0200

    🎉 feat: Init - Index page and config

---
+++ .vitepress/config.ts
@@ -0,0 +1,90 @@
+import { DefaultTheme, defineConfig, UserConfig } from 'vitepress'
+import { withSidebar } from 'vitepress-sidebar'
+
+const ROOT = 'notes'
+
+// https://vitepress.dev/reference/site-config
+const vitePressConfig: UserConfig = {
+	srcDir: ROOT,
+
+	title: "Andrei's brain",
+	description: 'Andrei Silin personal website',
+	head: [['link', { rel: 'icon', href: '/avatar.jpg' }]],
+	themeConfig: {
+		siteTitle: '@shestaya_liniya',
+		socialLinks: [
+			{ icon: 'github', link: 'https://github.com/shestaya_liniya' },
+			{ icon: 'telegram', link: 'https://t.me/shestaya_liniya' },
+		],
+		search: {
+			provider: 'local',
+		},
+	},
+	lastUpdated: true,
+}
+export default defineConfig(
+	withSidebar(vitePressConfig, {
+		/*
+		 * For detailed instructions, see the links below:
+		 * https://vitepress-sidebar.cdget.com/guide/options
+		 */
+		//
+		// ============ [ RESOLVING PATHS ] ============
+		documentRootPath: ROOT,
+		// scanStartPath: null,
+		// resolvePath: null,
+		// basePath: 'src/knowledge',
+		// followSymlinks: false,
+		//
+		// ============ [ GROUPING ] ============
+		// collapsed: true,
+		collapseDepth: 2,
+		// rootGroupText: 'Contents',
+		// rootGroupLink: 'https://github.com/jooy2',
+		// rootGroupCollapsed: false,
+		//
+		// ============ [ GETTING MENU TITLE ] ============
+		// useTitleFromFileHeading: true,
+		// useTitleFromFrontmatter: true,
+		// useFolderLinkFromIndexFile: false,
+		// useFolderTitleFromIndexFile: false,
+		// frontmatterTitleFieldName: 'title',
+		//
+		// ============ [ GETTING MENU LINK ] ============
+		// useFolderLinkFromSameNameSubFile: false,
+		// useFolderLinkFromIndexFile: false,
+		// folderLinkNotIncludesFileName: false,
+		//
+		// ============ [ INCLUDE / EXCLUDE ] ============
+		// excludeByGlobPattern: ['README.md', 'folder/'],
+		// excludeFilesByFrontmatterFieldName: 'exclude',
+		// includeDotFiles: false,
+		// includeEmptyFolder: false,
+		// includeRootIndexFile: false,
+		// includeFolderIndexFile: false,
+		//
+		// ============ [ STYLING MENU TITLE ] ============
+		// hyphenToSpace: true,
+		// underscoreToSpace: true,
+		// capitalizeFirst: false,
+		// capitalizeEachWords: false,
+		// keepMarkdownSyntaxFromTitle: false,
+		// removePrefixAfterOrdering: false,
+		// prefixSeparator: '.',
+		//
+		// ============ [ SORTING ] ============
+		// manualSortFileNameByPriority: ['first.md', 'second', 'third.md'],
+		// sortFolderTo: null,
+		// sortMenusByName: false,
+		// sortMenusByFileDatePrefix: false,
+		// sortMenusByFrontmatterOrder: false,
+		// frontmatterOrderDefaultValue: 0,
+		// sortMenusByFrontmatterDate: false,
+		// sortMenusOrderByDescending: false,
+		// sortMenusOrderNumericallyFromTitle: false,
+		// sortMenusOrderNumericallyFromLink: false,
+		//
+		// ============ [ MISC ] ============
+		// debugPrint: false,
+	}),
+)
---
+++ notes/index.md
@@ -0,0 +1,26 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: 'Andrei Silin'
+  tagline: 'Fullstack (front > back) developer who loves building beautiful apps.'
+
+  actions:
+    - theme: brand
+      text: Knowledge
+      link: /hello
+    - theme: alt
+      text: 'Why shestaya_liniya ?'
+      link: /hello
+  image:
+    src: 'public/avatar.jpg'
+    alt: Avatar
+---
+
+<p class="hero-p">
+  I have created this <a href="https://vitepress.dev/">space</a> for tracking and saving my knowledge in different fields, especially about dev. If you don't see something in my notes - <span class="accent">probably I don't know that.</span>
+</p>
+<p class="hero-p">
+  This space is open to everyone. So you can <a href="https://media1.giphy.com/media/v1.Y2lkPTZjMDliOTUyNGl0a3NvZWs5OW9hYzJzNDJ5OGZ5a21tN25vcHZ3eXMwZTl5eTdrYSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/dwmNhd5H7YAz6/source.gif">surf</a> freely here, and if you’d like to contribute, you’re welcome to click “Edit on GitHub” on any page.
+</p>
